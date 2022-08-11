6af28645095d6765ba5edac49db64cd85263813d
Farshad Amiri <47373990+FarshadAmiri@users.noreply.github.com>
2022-08-11 15:38:42 +0430

    Some Changes

--- Learning_Django/urls.py
+++ Learning_Django/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-from first_app.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', signup_view),
+    path('', include('first_app.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, HttpResponse
 def signup_view(request):
     name = request.GET.get('name')
     username = request.GET.get('username')
-    # return HttpResponse(f'You Successfully Signed up Dear {name}!\n Your username: {username}')
-    return HttpResponse('You Successfully Signed up')
+    return HttpResponse(f'You Successfully Signed up Dear {name}!<br> Your username: {username}')
+    # return HttpResponse('You Successfully Signed up')
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse('This is HomePage')
